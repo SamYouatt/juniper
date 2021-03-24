13c18f54918878a537fdb4753253441d6441826d
Sam Youatt <sdyouatt@btinternet.com>
2021-03-24 17:08:42 +0000

    added modal popup for rescheduling

--- app/components/TaskWidgetEditable.jsx
+++ app/components/TaskWidgetEditable.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import * as FileSystem from 'expo-file-system';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 
 import { View, Text, Button } from 'react-native';
 
 export default function TaskWidgetEditable({ fileName }) {
   const [task, setTask] = useState(null);
+  const [datePickerVisible, setDatePickerVisible] = useState(false);
+  const [timePickerVisible, setTimePickerVisible] = useState(false);
   const filePath = `${FileSystem.documentDirectory}tasks/${fileName}`;
 
   useEffect(() => {
@@ -25,20 +28,62 @@ export default function TaskWidgetEditable({ fileName }) {
     const deleted = await FileSystem.deleteAsync(filePath);
   };
 
+  const showDatePicker = () => {
+    setDatePickerVisible(true);
+  };
+
+  const showTimePicker = () => {
+    setTimePickerVisible(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisible(false);
+  };
+
+  const hideTimePicker = () => {
+    setTimePickerVisible(false);
+  };
+
+  const handleDateConfirm = async (date) => {
+    console.log(date);
+    hideDatePicker();
+    showTimePicker();
+  };
+
+  const handleTimeConfirm = async (time) => {
+    console.log(time);
+    hideTimePicker();
+  };
+
   return (
     <View>
       <Text>WIDGET</Text>
-      {task && (
-        <>
-          <Text>{task.name}</Text>
-          <Text>
-            Questions:
-            {' '}
-            {task.questions.length}
-          </Text>
-          <Button title="Delete" onPress={deleteFile} />
-        </>
-      )}
+      {task
+        ? (
+          <>
+            <Text>{task.name}</Text>
+            <Text>
+              Questions:
+              {' '}
+              {task.questions.length}
+            </Text>
+            <Button title="Delete" onPress={deleteFile} />
+            <Button title="Reschedule" onPress={showDatePicker} />
+            <DateTimePickerModal
+              isVisible={datePickerVisible}
+              mode="date"
+              onConfirm={handleDateConfirm}
+              onCancel={hideDatePicker}
+            />
+            <DateTimePickerModal
+              isVisible={timePickerVisible}
+              mode="time"
+              onConfirm={handleTimeConfirm}
+              onCancel={hideTimePicker}
+            />
+          </>
+        )
+        : <Text>Loading...</Text>}
 
     </View>
   );
