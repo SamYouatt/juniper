c1c2b1564e92d1235a9481d36e01d8a84f39dd94
Sam Youatt <sdyouatt@btinternet.com>
2021-03-24 15:29:35 +0000

    display task name and allow deleting

--- app/components/TaskWidgetEditable.jsx
+++ app/components/TaskWidgetEditable.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import * as FileSystem from 'expo-file-system';
+
+import { View, Text, Button } from 'react-native';
 
 export default function TaskWidgetEditable({ fileName }) {
+  const [task, setTask] = useState([]);
+  const filePath = `${FileSystem.documentDirectory}tasks/${fileName}`;
+
+  useEffect(() => {
+    readFile();
+  }, [task]);
+
+  const readFile = async () => {
+    try {
+      const fileContents = await FileSystem.readAsStringAsync(filePath).then();
+      const json = JSON.parse(fileContents);
+      setTask(json);
+    } catch {
+      setTask([]);
+    }
+  };
+
+  const deleteFile = async () => {
+    const deleted = await FileSystem.deleteAsync(filePath);
+  };
+
   return (
     <View>
       <Text>WIDGET</Text>
+      <Text>{task.name}</Text>
+      <Button title="Delete" onPress={() => deleteFile()} />
     </View>
   );
 }
