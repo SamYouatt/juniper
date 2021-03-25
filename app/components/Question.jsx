cd654158602a1a72c282e17e788dc88b9e3dbb80
Sam Youatt <sdyouatt@btinternet.com>
2021-03-25 14:24:42 +0000

    increment through quetsions

--- app/components/Question.jsx
+++ app/components/Question.jsx
@@ -1,17 +1,31 @@
 import React, { useState, useEffect } from 'react';
 
-import { View, Text, Button } from 'react-native';
+import {
+  View, Text, Button, Alert,
+} from 'react-native';
 
 export default function Question({ route, navigation }) {
   const { task } = route.params;
+  const numQuestions = task.questions.length;
+  console.log(task);
+  const [current, setCurrent] = useState(0);
 
   useEffect(() => {
     navigation.setOptions({ title: task !== null ? task.name : 'Task' });
   }, [task]);
 
+  const nextQuestion = () => {
+    if (current + 1 !== numQuestions) {
+      setCurrent(current + 1);
+    } else {
+      Alert.alert('NO MORE QUESTIONS');
+    }
+  };
+
   return (
     <View>
-      <Text>{task.name}</Text>
+      <Text>{task.questions[current].questionText}</Text>
+      <Button title="next" onPress={nextQuestion} />
     </View>
   );
 }
