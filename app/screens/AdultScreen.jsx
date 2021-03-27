feede57c84a6e54001fa56f6c59869887196db8b
Sam Youatt <sdyouatt@btinternet.com>
2021-03-27 12:22:04 +0000

    removed stuff that has been moved to other screens

--- app/screens/AdultScreen.jsx
+++ app/screens/AdultScreen.jsx
@@ -11,101 +11,13 @@ import TaskManager from './TaskManager';
 import SummaryScreen from './SummaryScreen';
 
 export default function AdultScreen({ navigation }) {
-  const [pin, setPin] = useState(null);
-  const [modalVisible, setModalVisible] = useState(false);
-
   const Tab = createBottomTabNavigator();
 
-  useEffect(() => {
-    getPin();
-  }, []);
-
-  const getPin = async () => {
-    try {
-      const value = await AsyncStorage.getItem('pin');
-      if (value !== null) {
-        setPin(value);
-      }
-    } catch (e) {
-      // error reading value
-      setPin(null);
-    }
-  };
-
-  const changePin = () => {
-    //
-    setModalVisible(true);
-    console.log('presed');
-  };
-
-  const addPin = () => {
-    //
-    setModalVisible(true);
-  };
-
-  const deletePin = async () => {
-    try {
-      await AsyncStorage.removeItem('pin');
-      setPin(null);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  const pinSubmitted = async (enteredPin) => {
-    try {
-      await AsyncStorage.setItem('pin', enteredPin);
-      setPin(enteredPin);
-      Alert.alert(null, 'Pin updated');
-      setModalVisible(false);
-    } catch (e) {
-      // saving error
-      console.log(e);
-    }
-  };
-
   return (
-  // <View>
     <Tab.Navigator>
       <Tab.Screen name="Settings" component={SettingsScreen} />
       <Tab.Screen name="Summary" component={SummaryScreen} />
       <Tab.Screen name="TaskMangement" component={TaskManager} />
     </Tab.Navigator>
-  // <Button
-  //   title="Settings"
-  //   onPress={() => {
-  //     navigation.navigate('Settings');
-  //   }}
-  // />
-  // <Button
-  //   title="Summary"
-  //   onPress={() => {
-  //     navigation.navigate('Summary');
-  //   }}
-  // />
-  // <Button
-  //   title="Manage Tasks"
-  //   onPress={() => {
-  //     navigation.navigate('TaskManager');
-  //   }}
-  // />
-  // {pin
-  //   ? <Button title="Change pin" onPress={changePin} />
-  //   : <Button title="Add pin" onPress={addPin} />}
-
-  // <Button title="delete pin" onPress={deletePin} />
-  // <Button title="test" onPress={() => console.log(pin)} />
-
-  // <Modal
-  //   animationType="slide"
-  //   transparent={false}
-  //   visible={modalVisible}
-  //   onRequestClose={() => setModalVisible(false)}
-  // >
-  //   <Text>Enter pincode</Text>
-  //   <PinCode onSubmit={pinSubmitted} />
-  //   <Button title="Dismiss" onPress={() => setModalVisible(false)} />
-  // </Modal>
-  // </View>
   );
 }
