eb5cff4618b8f0c7954f06ba479d9e89257b0cfa
Sam Youatt <sdyouatt@btinternet.com>
2021-03-27 15:23:30 +0000

    added all symbols to index

--- assets/images/symbols/SymbolsIndex.js
+++ assets/images/symbols/SymbolsIndex.js
@@ -23,8 +23,128 @@ const SymbolsIndex = {
   climbing: {
     uri: require('./climbing.png'),
   },
+  confused: {
+    uri: require('./confused.png'),
+  },
+  deer: {
+    uri: require('./deer.png'),
+  },
+  desert: {
+    uri: require('./desert.png'),
+  },
+  disappointed: {
+    uri: require('./disappointed.png'),
+  },
+  discouraged: {
+    uri: require('./discouraged.png'),
+  },
+  drive: {
+    uri: require('./drive.png'),
+  },
+  embarassed: {
+    uri: require('./embarassed.png'),
+  },
+  excited: {
+    uri: require('./excited.png'),
+  },
+  explore: {
+    uri: require('./explore.png'),
+  },
+  fish: {
+    uri: require('./fish.png'),
+  },
+  fishing: {
+    uri: require('./fishing.png'),
+  },
+  good: {
+    uri: require('./good.png'),
+  },
+  'grand-canyon': {
+    uri: require('./grand-canyon.png'),
+  },
+  hiking: {
+    uri: require('./hiking.png'),
+  },
+  hungry: {
+    uri: require('./hungry.png'),
+  },
+  islands: {
+    uri: require('./islands.png'),
+  },
+  lake: {
+    uri: require('./lake.png'),
+  },
+  leaf: {
+    uri: require('./leaf.png'),
+  },
+  meh: {
+    uri: require('./meh.png'),
+  },
+  'mount-rushmore': {
+    uri: require('./mount-rushmore.png'),
+  },
+  'mountain-biking': {
+    uri: require('./mountain-biking.png'),
+  },
+  mountains: {
+    uri: require('./mountains.png'),
+  },
+  nature: {
+    uri: require('./nature.png'),
+  },
+  nervous: {
+    uri: require('./nervous.png'),
+  },
+  'old-faithful': {
+    uri: require('./old-faithful.png'),
+  },
+  petals: {
+    uri: require('./petals.png'),
+  },
+  proud: {
+    uri: require('./proud.png'),
+  },
+  'redwood-forest': {
+    uri: require('./redwood-forest.png'),
+  },
+  relieved: {
+    uri: require('./relieved.png'),
+  },
+  river: {
+    uri: require('./river.png'),
+  },
+  scared: {
+    uri: require('./scared.png'),
+  },
+  sick: {
+    uri: require('./sick.png'),
+  },
+  silly: {
+    uri: require('./silly.png'),
+  },
   sky: {
-    uri: require('./beach.png'),
+    uri: require('./sky.png'),
+  },
+  surprised: {
+    uri: require('./surprised.png'),
+  },
+  swim: {
+    uri: require('./swim.png'),
+  },
+  tired: {
+    uri: require('./tired.png'),
+  },
+  trail: {
+    uri: require('./trail.png'),
+  },
+  tree: {
+    uri: require('./tree.png'),
+  },
+  underwater: {
+    uri: require('./underwater.png'),
+  },
+  volcano: {
+    uri: require('./volcano.png'),
   },
 };
 
