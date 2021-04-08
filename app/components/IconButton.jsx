dc90fe741991d767468964e6d0764e2f7f3b2a85
Sam Youatt <sdyouatt@btinternet.com>
2021-04-08 12:02:31 +0100

    💄 add margin to text

--- app/components/IconButton.jsx
+++ app/components/IconButton.jsx
@@ -1,9 +1,11 @@
 import { Feather } from '@expo/vector-icons';
 import React, { useContext } from 'react';
 
-import { View, Text, StyleSheet } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import Colours from '../../styles/Colours';
+import {
+  View, Text, StyleSheet, TouchableOpacity,
+} from 'react-native';
+// import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Colours, Spacing, Borders } from '../../styles/Index';
 import { SettingsContext } from '../config/SettingsContext';
 
 export default function IconButton({ icon, text, buttonAction }) {
@@ -23,17 +25,19 @@ const styles = StyleSheet.create({
   container: {
     // backgroundColor: Colours[settings.theme].mid,
     padding: 10,
-    borderRadius: 15,
+    borderRadius: Borders.radius.mid,
     minWidth: 150,
   },
   touchable: {
     flexDirection: 'row',
     alignItems: 'center',
+    height: 40,
   },
   text: {
     fontFamily: 'Helvetica',
     fontSize: 18,
     marginLeft: 10,
+    marginRight: Spacing.margin.small,
     // color: Colours[settings.theme].altdark,
     fontWeight: 'bold',
   },
